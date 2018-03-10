9136f7075390c2eb75672d8220fd6f0fec17d2b2
Harim <mark@wildmile.com>
2018-03-10 12:54:11 -0700

    Update

--- src/GUI/PythCalcGUI.java
+++ src/GUI/PythCalcGUI.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package GUI;
 
 /**
  *
- * @author mark
+ * @author Harim
  */
 public class PythCalcGUI extends javax.swing.JFrame {
 
@@ -121,7 +116,7 @@ public class PythCalcGUI extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void entryOneActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_entryOneActionPerformed
-        // TODO add your handling code here:
+        
     }//GEN-LAST:event_entryOneActionPerformed
 
     private void calcHypoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_calcHypoActionPerformed
@@ -267,11 +262,6 @@ public class PythCalcGUI extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
